ffb6cf3ad865d6a84488c7c645ae5e4d96abe922
Vladislav <vladined01@gmail.com>
2022-10-18 15:34:06 +0300

    f

--- educationProjectAngular/karma.conf.js
+++ educationProjectAngular/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/education-project-app'),
+      dir: require('path').join(__dirname, './coverage/education-project-angular'),
       subdir: '.',
       reporters: [
         { type: 'html' },
